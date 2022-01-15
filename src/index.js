876930171a443a5a8e3e005b79cf06add5ad4d07
Myat Ko Ko <coderinsider1@gmail.com>
2022-01-15 11:06:22 +0630

    React installaction

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+const getDOM = document.getElementById('mainpaenl');
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  getDOM
 );
 
 // If you want to start measuring performance in your app, pass a function
