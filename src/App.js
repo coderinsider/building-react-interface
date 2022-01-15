653cad9d69d9364f6417eae2c845ed3153927eb7
Myat Ko Ko <coderinsider1@gmail.com>
2022-01-15 21:44:39 +0630

    Adding components and sub components hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import  { BiArchive } from 'react-icons/bi';
-
+import Search from "./components/Search";
 const App = () => {
   return (
-    <h1>
-    <BiArchive />
-    Your Appointment
-    </h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+      <BiArchive className="inline-block text-red-800"/>
+      Your Appointment
+      </h1>
+      <Search />
+    </div>
   );
 }
 export default App;
