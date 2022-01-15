2853ecf908ea9ba5bdbda6f162a85e39d975038c
Myat Ko Ko <coderinsider1@gmail.com>
2022-01-15 21:54:18 +0630

    Getting appointments and debugging

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import  { BiArchive } from 'react-icons/bi';
 import Search from "./components/Search";
+import AddAppoint from "./components/AddAppointment";
 const App = () => {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl">
+      <h1 className="text-5xl mb-3">
       <BiArchive className="inline-block text-red-800"/>
       Your Appointment
       </h1>
+      <AddAppoint />
       <Search />
     </div>
   );
