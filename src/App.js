61a6d0b59397d0cae013b8a239a8cae94499aabe
Myat Ko Ko <coderinsider1@gmail.com>
2022-01-16 20:12:06 +0630

    Finished Application

--- src/App.js
+++ src/App.js
@@ -5,65 +5,65 @@ import AddAppoint from "./components/AddAppointment";
 // Getting Data JSON
 import AppointmentLists from './components/AppointmentLists';
 const App = () => {
-  let [appointLists, setAppointLists] = useState([]);
-  let [query, setQuery] = useState("");
-  let [sortBy, setSortBy] = useState("petName");
-  let [orderBy, setOrderBy] = useState('asc');
-  const filteredAppointments = appointLists.filter(
-    item => {
+   let [appointLists, setAppointLists] = useState([]);
+   let [query, setQuery] = useState("");
+   let [sortBy, setSortBy] = useState("petName");
+   let [orderBy, setOrderBy] = useState('asc');
+   const filteredAppointments = appointLists.filter(
+      item => {
+         return (
+            item.petName.toLowerCase().includes(query.toLowerCase()) ||
+            item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
+            item.aptNotes.toLowerCase().includes(query.toLowerCase())
+         )
+      }
+   ).sort((a,b) => {
+      let order = (orderBy == 'asc') ? 1: -1;
       return (
-        item.petName.toLowerCase().includes(query.toLowerCase()) ||
-        item.ownerName.toLowerCase().includes(query.toLowerCase()) ||
-        item.aptNotes.toLowerCase().includes(query.toLowerCase())
+         a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
       )
-    }
-  ).sort((a,b) => {
-    let order = (orderBy == 'asc') ? 1: -1;
-    return (
-      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
-    )
-  });
-  const fetchData = useCallback(() => {
-    fetch('./data.json')
-    .then(resp => resp.json())
-    .then(data => {
-      setAppointLists(data);
-    })
-  }, []);
-  useEffect(() => {
-    fetchData()
-  }, [fetchData]);
-  return (
-    <div className="App container mx-auto mt-3 font-thin">
-      <h1 className="text-5xl mb-3">
-      <BiArchive className="inline-block text-red-800"/>
-      Your Appointment
-      </h1>
-      <AddAppoint
-      onSendAppointment={myAppointment => setAppointLists([...appointLists, myAppointment])}
-      lastId={appointLists.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
-      />
-      <Search query={query}
-        onQueryChange={myQuery => setQuery(myQuery)}
-        orderBy={orderBy}
-        onOrderByChange={mySort => setOrderBy(mySort)}
-        sortBy={sortBy}
-        onSortByChange={mySort => setSortBy(mySort)}
-      />
+   });
+   const fetchData = useCallback(() => {
+      fetch('./data.json')
+      .then(resp => resp.json())
+      .then(data => {
+         setAppointLists(data);
+      })
+   }, []);
+   useEffect(() => {
+      fetchData()
+   }, [fetchData]);
+   return (
+      <div className="App container mx-auto mt-3 font-thin">
+         <h1 className="text-5xl mb-3">
+            <BiArchive className="inline-block text-red-800"/>
+            Your Appointment
+         </h1>
+         <AddAppoint
+            onSendAppointment={myAppointment => setAppointLists([...appointLists, myAppointment])}
+            lastId={appointLists.reduce((max, item) => Number(item.id) > max ? Number(item.id) : max, 0)}
+         />
+         <Search query={query}
+            onQueryChange={myQuery => setQuery(myQuery)}
+            orderBy={orderBy}
+            onOrderByChange={mySort => setOrderBy(mySort)}
+            sortBy={sortBy}
+            onSortByChange={mySort => setSortBy(mySort)}
+         />
 
-      <ul className="divide-y divide-gray-200">
-        {filteredAppointments.map(data => (
-          <AppointmentLists 
-            key={data.id} 
-            data={data}
-            onDeleteAppointment={
-              appointmentId =>
+         <ul className="divide-y divide-gray-200">
+            {filteredAppointments.map(data => (
+            <AppointmentLists 
+               key={data.id} 
+               data={data}
+               onDeleteAppointment={
+               appointmentId =>
                 setAppointLists(appointLists.filter(data => data.id != appointmentId))
-            }
-          />
-        ))}
-      </ul>
-    </div>
-  );
+               }
+            />
+            ))}
+         </ul>
+      </div>
+   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 const getDOM = document.getElementById('mainpaenl');
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  getDOM
+   <React.StrictMode>
+      <App />
+   </React.StrictMode>,
+   getDOM
 );
 
 
