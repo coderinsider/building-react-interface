bc55973ecc4d90d4d335596b3f664974456fb421
Myat Ko Ko <coderinsider1@gmail.com>
2022-01-16 16:08:04 +0630

    Setting up a sort

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import AppointmentLists from './components/AppointmentLists';
 const App = () => {
   let [appointLists, setAppointLists] = useState([]);
   let [query, setQuery] = useState("");
-    const filteredAppointments = appointLists.filter(
+  let [sortBy, setSortBy] = useState("petName");
+  let [orderBy, setOrderBy] = useState('asc');
+  const filteredAppointments = appointLists.filter(
     item => {
       return (
         item.petName.toLowerCase().includes(query.toLowerCase()) ||
@@ -15,7 +17,12 @@ const App = () => {
         item.aptNotes.toLowerCase().includes(query.toLowerCase())
       )
     }
-  );
+  ).sort((a,b) => {
+    let order = (orderBy == 'desc') ? 1: -1;
+    return (
+      a[sortBy].toLowerCase() < b[sortBy].toLowerCase() ? -1 * order : 1 * order
+    )
+  });
   const fetchData = useCallback(() => {
     fetch('./data.json')
     .then(resp => resp.json())
